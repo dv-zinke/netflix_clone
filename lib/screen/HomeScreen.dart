87b07e0e034d35fdb81830038cc8c0d1d0db7639
hj <dv.zinke@gmail.com>
2020-05-23 16:40:38 +0900

    feat : 무비 모델 추가

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/model/Movie.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': "사랑의 불시착",
+      'keyword': "사랑/로맨틱/판타지",
+      "poster" : "test_movie_1.png",
+      "like": false
+    })
+  ];
+
   @override
   void initState() {
     super.initState();
