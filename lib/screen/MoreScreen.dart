84d2ebf8089fa14f419a3005cb40521021e2a320
hj <dv.zinke@gmail.com>
2020-05-31 23:43:48 +0900

    feat : linkfiy, url_launcher 추가 했는데 에러남 수정해야함

--- lib/screen/MoreScreen.dart
+++ lib/screen/MoreScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MoreScreen extends StatelessWidget {
   @override
@@ -13,6 +15,34 @@ class MoreScreen extends StatelessWidget {
               radius: 100,
               backgroundImage: AssetImage("images/logo.png"),
             ),
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              child: Text(
+                "HeeJune",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 140,
+              height: 5,
+              color: Colors.red,
+            ),
+            Container(
+                padding: EdgeInsets.all(10),
+                child: Linkify(
+                  onOpen: (link) async {
+                    if (await canLaunch(link.url)) {
+                      await launch(link.url);
+                    }
+                  },
+                  text: "https://github.com/dv-zinke",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  linkStyle: TextStyle(color: Colors.white),
+                ))
           ],
         ),
       ),
