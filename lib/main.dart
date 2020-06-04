770e26aac37faa5ec98d94a5b866236ff8231474
hj <dv.zinke@gmail.com>
2020-06-04 23:52:08 +0900

    feat : search 스크린 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/screen/HomeScreen.dart';
 import 'package:netflix_clone/screen/MoreScreen.dart';
+import 'package:netflix_clone/screen/SearchScreen.dart';
 import 'package:netflix_clone/widget/BottomBar.dart';
 
 void main() => runApp(MyApp());
@@ -27,9 +28,7 @@ class _MyAppState extends State<MyApp> {
             physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
               HomeScreen(),
-              Container(
-                child: Center(child: Text("검색")),
-              ),
+              SearchScreen(),
               Container(
                 child: Center(child: Text("저장한 컨텐츠 목록")),
               ),
